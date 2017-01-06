de6d7418d12f82125cae39c48992e87ab55bc7ce
varun <varun.bhargava80@gmail.com>
2017-01-06 21:07:36 +1100

    initial commit for source control

---
+++ js/site.js
@@ -0,0 +1,95 @@
+/*global $*/
+
+$(document).ready(function () {
+
+    "use strict";
+     
+    $('.ws-slider').slick({
+        autoplay: true,
+        arrows: true,
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+    
+        ]
+    });
+
+
+
+    $("#scroll-logos").simplyScroll();
+  
+
+    /**
+      Modified by Varun
+    */
+    
+    $('.offer-img, .offer-box2').mouseenter(function () {
+        $(this).parents('.offer-card').addClass('rotate');
+    });
+
+    $('.offer-card').mouseleave(function () {
+        $(this).removeClass('rotate');
+    });
+    
+    
+    /* Start Varun Japan Edits */
+    if ($(window).width() <= 767) {
+        $('.offer-card').click(function () {
+            $(this).toggleClass('rotate');
+        });
+        $('.offer-img').click(function () {
+            $(this).parents('.offer-card').toggleClass('rotate');
+        });
+    }
+    $(".offer-card-outer > .offer-card").hover(function () {
+        if (($(this).find(".card-details")).is(":only-of-type")) {
+            $(this).find(".card-details").css("min-height", "200px");
+        } else {
+            $(this).css("height", "550px");
+        }
+    }, function () {
+        $(this).css("height", "320");
+    });
+    /* End Varun Japan Edits */
+
+    
+    // ----- TILE -----
+    $(".card-back .offer-box2").click(function () {
+        $(".added-case:not(#" + $(this).data("added-case") + ")").hide();
+
+        $("#" + $(this).data("added-case")).toggle(500);
+    });
+    // ----- TILE -----
+
+});
+
+
+
+     
